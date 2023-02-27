422afe4293241cba4af36a3c3762acfe10572d3f
JimmyLu <luu21@kimo.com>
2023-02-28 02:08:51 +0800

    Change some setting

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@ defineProps({
 
 const count = ref(0)
 const name = 1
-const items = [{message:"asdf"},{message:"asdf"},{message:"asdf"},{message:"asdf"},{message:"asdf"}]
+const items = [{message:"1"},{message:"2"},{message:"3"},{message:"4"},{message:"5"}]
 function test(){  
   return count.value %2
 }
@@ -19,29 +19,19 @@ function test(){
   <li v-for="item in items">
   {{ item.message }}
   </li>
-  <p v-if= "test()" >還剩下21天！</p>
+  <p v-if= "test()" >GoGo</p>
 
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
   <p>
     Install
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
     in your IDE for a better DX
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p class="read-the-docs">@2023 Jimmy Lu</p>
 </template>
 
 <style scoped>
