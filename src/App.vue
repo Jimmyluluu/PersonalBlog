20998085684b23dc3f737f86a9edad05dabcae7f
root <root@TWTCL2SYC6D3.WestPharma.net>
2023-03-08 18:29:10 +0800

    template

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,68 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <div></div>
-  <HelloWorld msg="Personal" />
+  <div>
+    <header class="bg-gray-900 text-white">
+      <h1 class="text-3xl font-bold">{{ title }}</h1>
+      <nav>
+        <ul class="inline-flex">
+          <li><a href="#" class="px-4 py-2 hover:bg-gray-800">Home</a></li>
+          <li><a href="#" class="px-4 py-2 hover:bg-gray-800">About</a></li>
+          <li><a href="#" class="px-4 py-2 hover:bg-gray-800">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <main class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <article class="bg-white shadow overflow-hidden sm:rounded-lg mb-8">
+        <div class="px-4 py-5 sm:px-6">
+          <h2 class="text-2xl font-bold mb-2">{{ post1.title }}</h2>
+          <p class="text-sm text-gray-500 mb-2">Author: {{ post1.author }} | Date: {{ post1.date }}</p>
+        </div>
+        <div class="px-4 py-5 sm:p-6">
+          <p class="text-base">{{ post1.content }}</p>
+          <a href="#" class="text-blue-600 font-bold hover:underline">Read more...</a>
+        </div>
+      </article>
+
+      <article class="bg-white shadow overflow-hidden sm:rounded-lg mb-8">
+        <div class="px-4 py-5 sm:px-6">
+          <h2 class="text-2xl font-bold mb-2">{{ post2.title }}</h2>
+          <p class="text-sm text-gray-500 mb-2">Author: {{ post2.author }} | Date: {{ post2.date }}</p>
+        </div>
+        <div class="px-4 py-5 sm:p-6">
+          <p class="text-base">{{ post2.content }}</p>
+          <a href="#" class="text-blue-600 font-bold hover:underline">Read more...</a>
+        </div>
+      </article>
+    </main>
+
+    <footer class="bg-gray-900 text-white text-center py-4">
+      <p>&copy; 2023 My Blog</p>
+    </footer>
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+export default {
+  data() {
+    return {
+      title: 'My Blog',
+      post1: {
+        title: 'First Blog',
+        author: 'John Doe',
+        date: 'March 1, 2023',
+        content: 'Lorem ipsum dolor sit amet.'
+      },
+      post2: {
+        title: 'Second Blog',
+        author: 'Jane Smith',
+        date: 'March 3, 2023',
+        content: 'Praesent ac risus vel odio.'
+      }
+    }
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+</script>
+
+<style>
+/* 在這裡繼續使用 Tailwind CSS 的樣式 */
 </style>
