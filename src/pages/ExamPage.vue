908abff86bfd8dda20a3995d77d21893ccbe8bd9
Jimmyluluu <luu21@kimo.com>
2023-06-26 10:35:53 +0800

    add exampage

---
+++ src/pages/ExamPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Markdown :source="source" />
+    <Markdown :source="Test" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Markdown from 'vue3-markdown-it';
+
+const source = ref('- Hello World!');
+</script>
