8fe12a5c7a429eb3387f4d1c47edcca5046c18aa
Jimmyluluu <luu21@kimo.com>
2023-07-06 14:36:09 +0800

    update home and exam

--- src/pages/ExamPage.vue
+++ src/pages/ExamPage.vue
@@ -1,6 +1,21 @@
 <template>
-  <div>
-    <button class="btn btn-primary">Button</button>
+  <div class="px-10 py-10">
+    <table class="border-collapse border border-spacing-2 border-slate-500 hover:table-auto">
+      <thead>
+        <tr>
+          <th class="border border-slate-600 text-gray-800">7-9</th>
+          <th class="border border-slate-600 text-gray-800">10-11</th>
+          <th class="border border-slate-600 text-gray-800">12-1</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="border border-slate-700 text-gray-800">把課程都聽完</td>
+          <td class="border border-slate-700 text-gray-800">讀第二遍做筆記</td>
+          <td class="border border-slate-700 text-gray-800">瘋狂寫考古題訂正</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="bg-auto">
     <div class="row">
-      <div class="mx-auto py-4"></div>
-      <div class="flex flex-col gap-5">
-        <section v-for="post in posts" class="px-4 sm:px-6 lg:px-8">
-          <article class="bg-white shadow overflow-hidden rounded-lg pb-8">
-            <h1>hello</h1>
-          </article>
-        </section>
-      </div>
+      <article class="prose max-w-none justify-center px-10 py-10">
+        <h2 class="text-gray-800">
+          大家好～ 歡迎來到我的 Blog，我是 Jimmy
+          ，因為目前是考研戰士，所以沒時間去完成它，如果有人在 Follow
+          的話請耐心等候，二月考完會來處理的～
+        </h2>
+      </article>
     </div>
   </div>
 </template>
