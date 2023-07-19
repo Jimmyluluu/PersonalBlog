98be70650ae1a3caf80bc0b66f78f131ca5bcee8
Jimmyluluu <luu21@kimo.com>
2023-07-19 16:31:01 +0800

    try to fix mobile will show NaN on exam page

--- src/pages/ExamPage.vue
+++ src/pages/ExamPage.vue
@@ -38,14 +38,17 @@ const number = ref(1);
 onMounted(updateNumber);
 
 function updateNumber() {
-  const currentDate = new Date();
-  const year = currentDate.getFullYear();
-  const month = currentDate.getMonth() + 1;
-  const day = currentDate.getDate();
-  const dateString = `${year},${month},${day}`;
-  const timestamp = new Date(dateString).getTime() / 1000;
-  const days = Math.floor((timestamp - 1688313600) / (24 * 60 * 60))
+  const currentTimeStamp = Math.floor(Date.now() / 1000);
+  const days = Math.floor((currentTimeStamp - 1688313600) / (24 * 60 * 60));
   number.value = number.value + days;
 }
 </script>
-<style>  iframe { width: 100%; height: 500px; border: 2px solid #ccc; border-radius: 10px; padding: none; }  </style>
+<style>
+iframe {
+  width: 100%;
+  height: 500px;
+  border: 2px solid #ccc;
+  border-radius: 10px;
+  padding: none;
+}
+</style>
